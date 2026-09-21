fc7670a37e903439aed20cf25eb454417e44f6a0
DinhTan5K <tanv3660@gmail.com>
2026-09-21 12:39:59 +0700

    restruc

--- backend/PaperLessApi/Program.cs
+++ backend/PaperLessApi/Program.cs
@@ -3,25 +3,30 @@ using PaperLessApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
-// Đăng ký Repository và Service (Clean Architecture theo ý bro)
-builder.Services.AddScoped<PaperLessApi.Repositories.IInvoiceRepository, PaperLessApi.Repositories.InvoiceRepository>();
-builder.Services.AddScoped<PaperLessApi.Services.IInvoiceService, PaperLessApi.Services.InvoiceService>();
+builder.Services.AddScoped<
+    PaperLessApi.Repositories.IInvoiceRepository,
+    PaperLessApi.Repositories.InvoiceRepository>();
 
+builder.Services.AddScoped<
+    PaperLessApi.Services.IInvoiceService,
+    PaperLessApi.Services.InvoiceService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
